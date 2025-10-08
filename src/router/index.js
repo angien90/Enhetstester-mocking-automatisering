bf6e5191fb4012f5ae5f0b50fad4f1d5ccc2acfd
angien90 <angelicanylander@hotmail.com>
2025-10-08 17:26:21 +0200

    Bytte tillbaka index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Cart from '../views/Cart.vue'
 import AboutUs from '../views/AboutUs.vue'
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
